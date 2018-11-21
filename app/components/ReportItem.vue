2f34961d58811f04a673ba6679a478ac57d45a30
mottox2 <y.mottox2@gmail.com>
2018-11-21 22:11:46 +0900

    Add ReportItem.vue

---
+++ app/components/ReportItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="base">
+    <div
+      class="image"
+      style="background-image: url(https://dummyimage.com/360x240/aaa/fff)"
+    />
+    <div class="contents">
+      <div class="date">2018.11.22</div>
+      <h3 class="title">秋の夜長の自由研究LT大会</h3>
+      <p class="description">
+        秋が深まり、夜が長くなってきましたね。<br>
+        エンジニアの皆さんはそんな夜に何をされていますか？<br>
+        コードを書いたり技術書を読んだりもいいですが、やっぱり自由なテーマでLTをするのが楽しいですよね。...
+      </p>
+      <div class="button">
+        イベントページへ
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.base {
+  display: flex;
+}
+
+.image {
+  width: 380px;
+  height: 0;
+  padding-bottom: 25%;
+  background-position: center center;
+  background-repeat: no-repeat;
+  margin-right: 20px;
+}
+
+.date {
+  font-size: 24px;
+  color: #eb011e;
+  font-weight: 600;
+}
+
+.title {
+  font-size: 32px;
+  font-weight: 600;
+}
+
+.description {
+  font-size: 14px;
+  margin-top: 8px;
+}
+
+.button {
+  background-color: #eb011e;
+  display: inline-block;
+  padding: 12px 48px;
+  border-radius: 36px;
+  font-weight: 600;
+  color: white;
+  margin-top: 12px;
+  font-size: 14px;
+  margin-top: 16px;
+}
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,6 +5,8 @@
 </template>
 
 <style>
+@import 'minireset.css';
+
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
