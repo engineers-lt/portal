f15c53c1e16846e539bc29e7828594fd89f26a72
mottox2 <y.mottox2@gmail.com>
2019-01-15 03:48:31 +0900

    Use gridsome

---
+++ gridsome.config.js
@@ -0,0 +1,10 @@
+// This is where project configuration and installed plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here requires a server restart to work properly.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: []
+}
