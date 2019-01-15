f753c61d8941552cd236ea1d792dd076875e1d3f
mottox2 <y.mottox2@gmail.com>
2019-01-15 21:18:34 +0900

    Tweak

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <header class="header">
+    <div class="container">
+      <g-link to='/'><img src="/logo.png" width="216" height="58" :alt="$static.metaData.siteName"  /></g-link>
+      <nav class="headerNav">
+        <g-link to='/about'>About</g-link>
+        <g-link to='/guideline'>Guideline</g-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<style scoped>
+.header .container {
+  display: flex;
+  align-items: center;
+}
+
+.headerNav {
+  margin-left: 24px;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 600;
+}
+
+.headerNav a {
+  color: #444;
+  text-decoration: none;
+  padding: 12px 12px;
+  font-size: 15px;
+}
+
+.headerNav a:hover {
+  color: #30A192;
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="layout">
-    <header class="header">
+    <app-header/>
+    <!-- <header class="header">
       <div class="container">
         <g-link to='/'><img src="/logo.png" width="216" height="58" :alt="$static.metaData.siteName"  /></g-link></div>
         <g-link to='/about'>About</g-link>
-    </header>
+    </header> -->
     <slot/>
   </div>
 </template>
 
-<static-query>
-query {
-  metaData {
-    siteName
+<script>
+import Header from '~/components/Header.vue'
+
+export default {
+  components: {
+    AppHeader: Header
   }
 }
-</static-query>
+</script>
 
 <style>
 /*! minireset.css v0.0.3 | MIT License | github.com/jgthms/minireset.css */html,body,p,ol,ul,li,dl,dt,dd,blockquote,figure,fieldset,legend,textarea,pre,iframe,hr,h1,h2,h3,h4,h5,h6{margin:0;padding:0}h1,h2,h3,h4,h5,h6{font-size:100%;font-weight:normal}ul{list-style:none}button,input,select,textarea{margin:0}html{box-sizing:border-box}*,*:before,*:after{box-sizing:inherit}img,embed,iframe,object,audio,video{height:auto;max-width:100%}iframe{border:0}table{border-collapse:collapse;border-spacing:0}td,th{padding:0;text-align:left}
